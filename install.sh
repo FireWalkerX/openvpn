94fdd693a37f2465ea7a80b20b265c9b55684a39
jonathanwalker <jonathanwalker@hush.com>
2015-10-08 14:27:28 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,8 +5,7 @@ mkdir install
 cd install
 wget https://raw.githubusercontent.com/jonathanwalker/openvpn/master/unbound.sh > /dev/null 2>&1
 wget https://raw.githubusercontent.com/jonathanwalker/openvpn/master/adblocker.sh > /dev/null 2>&1
-wget https://raw.githubusercontent.com/jonathanwalker/openvpn/master/setup.sh > /dev/null 2>&1
+wget https://raw.githubusercontent.com/jonathanwalker/openvpn/master/openvpn.sh > /dev/null 2>&1
 sh unbound.sh
 sh adblocker.sh
 cd ..
-rm -rf install
