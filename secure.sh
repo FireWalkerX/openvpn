bfe46627a7d0269ea00e1f08ca9d104cc60ee786
jonathanwalker <jonathanwalker@hush.com>
2015-10-17 14:52:49 -0400

    Delete secure.sh

--- secure.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-yum install -y perl > /dev/null 2>&1
-wget http://www.configserver.com/free/csf.tgz
-tar -xzf csf.tgz > /dev/null 2>&1
-cd csf
-bash install.sh > /dev/null 2>&1
-perl /usr/local/csf/bin/csftest.pl
