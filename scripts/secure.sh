dab24ddfe7763b8e5a3c9cd564c0ed076d68f162
jonathanwalker <jonathanwalker@hush.com>
2015-10-20 20:12:23 -0400

    Update secure.sh

--- scripts/secure.sh
+++ scripts/secure.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
 
-echo "[-] Securing the Server"
-
 #Install Dependancies
-echo "Installing Dependancies..."
 yum install -y net-tools > /dev/null 2>&1
 yum install -y perl > /dev/null 2>&1
 yum install -y bind-utils > /dev/null 2>&1
 
 # Configure Cron Jobs
-echo "Configuring Cron Jobs..."
 echo "00 00 * * * yum -y update" >> /tmp/securecronjob
 crontab /tmp/securecronjob
 rm -f /tmp/securecronjob
 
 # Install ConfigServer Firewall
-echo "Installing and Installing ConfigserverFirewall..."
 wget http://www.configserver.com/free/csf.tgz  > /dev/null 2>&1
 tar -xzf csf.tgz > /dev/null 2>&1
 cd csf
@@ -27,4 +22,4 @@ rm -f /etc/csf/csf.conf
 cp -fp resources/csf.conf /etc/csf/csf.conf
 csf -r  > /dev/null 2>&1
 
-echo "[+] Server is Hardened!"
+echo "[+] Server hardening complete"
