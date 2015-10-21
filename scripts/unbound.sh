995f88ad333ef31c69b7f9c17453c0fb3be0d825
jonathanwalker <jonathanwalker@hush.com>
2015-10-20 20:16:09 -0400

    Update unbound.sh

--- scripts/unbound.sh
+++ scripts/unbound.sh
@@ -3,12 +3,9 @@
 # Written by JonathanW
 
 # Install Unbound
-echo "[-] Installing and Configuring Unbound"
-echo "Installing unbound..."
 yum install -y unbound > /dev/null 2>&1
 
 # Grab Configuration File
-echo "Retrieving and setting configuration file..."
 > /etc/unbound/unbound.conf
 cp resources/unbound.conf /etc/unbound/unbound.conf
 unbound-control-setup  > /dev/null 2>&1
@@ -16,17 +13,16 @@ chown unbound:root /etc/unbound/unbound_*
 chmod 440 /etc/unbound/unbound_*
 
 # Retrieve primary root DNS servers for root hint validation
-echo "Retrieving root hints for validation..."
 wget ftp://ftp.internic.net/domain/named.cache -O /etc/unbound/named.cache  > /dev/null 2>&1
 unbound-anchor -r /etc/unbound/named.cache  > /dev/null 2>&1
 
-echo "Restarting and Enabling Unbound service..."
+# Restart unbound and enable the service
 systemctl restart unbound.service  > /dev/null 2>&1
 sudo systemctl enable unbound.service  > /dev/null 2>&1
 
-echo "Configuring cronjobs..."
+# Configure cron jobs to dump the cache every 24 hours
 crontab -l > /tmp/cronjob
 echo "00 00 * * * unbound-control dump_cache > /tmp/DNS_cache.txt" >> /tmp/cronjob
 crontab /tmp/cronjob
 rm -f /tmp/cronjob
-echo "[+] Installation of Unbound Complete!"
+echo "[+] Installation of Unbound complete"
