fe2d1a61c6e34ffc2eb63de4776b53bf2aa5147b
jonathanwalker <jonathanwalker@hush.com>
2015-10-20 20:48:57 -0400

    Update secure.sh

--- scripts/secure.sh
+++ scripts/secure.sh
@@ -4,7 +4,7 @@
 yum install -y net-tools > /dev/null 2>&1
 yum install -y perl > /dev/null 2>&1
 yum install -y bind-utils > /dev/null 2>&1
-yum install perl-libwww-perl net-tools perl-LWP-Protocol-https > /dev/null 2>&1
+#yum install perl-libwww-perl net-tools perl-LWP-Protocol-https > /dev/null 2>&1
 
 # Configure Cron Jobs
 echo "00 00 * * * yum -y update" >> /tmp/securecronjob
@@ -12,17 +12,17 @@ crontab /tmp/securecronjob
 rm -f /tmp/securecronjob
 
 # Install ConfigServer Firewall
-wget http://www.configserver.com/free/csf.tgz  > /dev/null 2>&1
-tar -xzf csf.tgz > /dev/null 2>&1
-cd csf
-bash install.sh > /dev/null 2>&1
-cd ..
-rm -rf csf/
-rm -f csf.tgz
-rm -f /etc/csf/csf.conf
-cp -fp resources/csf.conf /etc/csf/csf.conf
-csf -r  > /dev/null 2>&1
-useradd csf
+#wget http://www.configserver.com/free/csf.tgz  > /dev/null 2>&1
+#tar -xzf csf.tgz > /dev/null 2>&1
+#cd csf
+#bash install.sh > /dev/null 2>&1
+#cd ..
+#rm -rf csf/
+#rm -f csf.tgz
+#rm -f /etc/csf/csf.conf
+#cp -fp resources/csf.conf /etc/csf/csf.conf
+#csf -r  > /dev/null 2>&1
+#useradd csf
 
 # Setup User
 adduser $superUser
