c707b0de19abcd902c9b14f261df82f9ff6e7209
jonathanwalker <jonathanwalker@hush.com>
2015-11-17 19:45:57 -0500

    Update client.sh

--- scripts/client.sh
+++ scripts/client.sh
@@ -26,7 +26,7 @@ echo "script-security 1" >> /home/$superUser/$clientDevice.ovpn
 echo "cipher AES-256-CBC" >> /home/$superUser/$clientDevice.ovpn
 echo "auth SHA512" >> /home/$superUser/$clientDevice.ovpn
 echo "remote-cert-eku \"TLS Web Server Authentication\"" >> /home/$superUser/$clientDevice.ovpn
-echo "verify-x509-name 'C=$country, ST=$province, L=$city, O=$organization, OU=$organizationUnit, CN=server, name=server, emailAddress=$email'" >> /home/$superUser/$clientDevice.ovpn
+echo "verify-x509-name 'C=$country, ST=$province, L=$city, O=$organization, OU=$organizationUnit, CN=$commonName, name=$commonName, emailAddress=$email'" >> /home/$superUser/$clientDevice.ovpn
 echo "dhcp-option DNS 10.8.0.1" >> /home/$superUser/$clientDevice.ovpn
 echo "<ca>" >> /home/$superUser/$clientDevice.ovpn
 cat /etc/openvpn/easy-rsa/keys/ca.crt >> /home/$superUser/$clientDevice.ovpn
